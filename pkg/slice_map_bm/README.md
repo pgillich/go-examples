d53b3a89fc4f46b11ed3490b237ded111ca45100
pgillich <pgillich@gmail.com>
2023-05-22 21:14:19 +0200

    BM with GC

--- pkg/slice_map_bm/README.md
+++ pkg/slice_map_bm/README.md
@@ -5,3 +5,8 @@
 clear; go test -bench='.' -run='^#' -count=1 -benchtime 10s -gcflags "-N -m -l" 2>result_1.deb | tee result_1.txt
 
 clear; go test -bench='.' -run='^#' -count=5 -benchtime 10s -gcflags "-N -m -l" 2>result_5.deb | tee result_5.txt
+
+benchstat -filter '.name:/Fill/' -col /size result_5.txt > result_5_Fill.txt
+benchstat -filter '.name:/Seek/' -col /size result_5.txt > result_5_Seek.txt
+
+clear; go test -bench='.' -run='^#' -count=10 -benchtime 10s -timeout 30m -gcflags "-N -m -l" 2>result_10.deb | tee result_10.txt
--- pkg/slice_map_bm/bm_test.go
+++ pkg/slice_map_bm/bm_test.go
@@ -4,6 +4,7 @@ import (
 	crand "crypto/rand"
 	"fmt"
 	mrand "math/rand"
+	"runtime"
 	"testing"
 )
 
@@ -11,7 +12,9 @@ const (
 	strLenMin = 5
 	strLenMax = 50
 
-	sliceSeekLimit = 50000
+	sliceSeekLimit = 20000
+
+	doGC = true
 )
 
 var (
@@ -21,19 +24,33 @@ var (
 func BenchmarkSliceDynFill(b *testing.B) {
 	for _, size := range testSizes {
 		b.Run(fmt.Sprintf("size=%d", size), func(b *testing.B) {
-			items := sliceFillDyn(size)
-			_ = sliceUse(items)
+			benchmarkSliceDynFill(b, size)
+			if doGC {
+				runtime.GC()
+			}
 		})
 	}
+}
+
+func benchmarkSliceDynFill(b *testing.B, size int) {
+	items := sliceFillDyn(size)
+	_ = sliceUse(items)
 }
 
 func BenchmarkSliceFixFill(b *testing.B) {
 	for _, size := range testSizes {
 		b.Run(fmt.Sprintf("size=%d", size), func(b *testing.B) {
-			items := sliceFillFix(size)
-			_ = sliceUse(items)
+			benchmarkSliceFixFill(b, size)
+			if doGC {
+				runtime.GC()
+			}
 		})
 	}
+}
+
+func benchmarkSliceFixFill(b *testing.B, size int) {
+	items := sliceFillFix(size)
+	_ = sliceUse(items)
 }
 
 func BenchmarkSliceDynSeek(b *testing.B) {
@@ -42,13 +59,20 @@ func BenchmarkSliceDynSeek(b *testing.B) {
 			continue
 		}
 		b.Run(fmt.Sprintf("size=%d", size), func(b *testing.B) {
-			b.StopTimer()
-			items := sliceFillDyn(size)
-			b.StartTimer()
-			for s := 0; s < size; s++ {
-				_ = sliceSeek(items, allStrings[s])
+			benchmarkSliceDynSeek(b, size)
+			if doGC {
+				runtime.GC()
 			}
 		})
+	}
+}
+
+func benchmarkSliceDynSeek(b *testing.B, size int) {
+	b.StopTimer()
+	items := sliceFillDyn(size)
+	b.StartTimer()
+	for s := 0; s < size; s++ {
+		_ = sliceSeek(items, allStrings[s])
 	}
 }
 
@@ -58,57 +82,92 @@ func BenchmarkSliceFixSeek(b *testing.B) {
 			continue
 		}
 		b.Run(fmt.Sprintf("size=%d", size), func(b *testing.B) {
-			b.StopTimer()
-			items := sliceFillFix(size)
-			b.StartTimer()
-			for s := 0; s < size; s++ {
-				_ = sliceSeek(items, allStrings[s])
+			benchmarkSliceFixSeek(b, size)
+			if doGC {
+				runtime.GC()
 			}
 		})
+	}
+}
+
+func benchmarkSliceFixSeek(b *testing.B, size int) {
+	b.StopTimer()
+	items := sliceFillFix(size)
+	b.StartTimer()
+	for s := 0; s < size; s++ {
+		_ = sliceSeek(items, allStrings[s])
 	}
 }
 
 func BenchmarkMapDynFill(b *testing.B) {
 	for _, size := range testSizes {
 		b.Run(fmt.Sprintf("size=%d", size), func(b *testing.B) {
-			items := mapFillDyn(size)
-			_ = mapUse(items)
+			bBenchmarkMapDynFill(b, size)
+			if doGC {
+				runtime.GC()
+			}
 		})
 	}
+}
+
+func bBenchmarkMapDynFill(b *testing.B, size int) {
+	items := mapFillDyn(size)
+	_ = mapUse(items)
 }
 
 func BenchmarkMapFixFill(b *testing.B) {
 	for _, size := range testSizes {
 		b.Run(fmt.Sprintf("size=%d", size), func(b *testing.B) {
-			items := mapFillFix(size)
-			_ = mapUse(items)
+			benchmarkMapFixFill(b, size)
+			if doGC {
+				runtime.GC()
+			}
 		})
 	}
+}
+
+func benchmarkMapFixFill(b *testing.B, size int) {
+	items := mapFillFix(size)
+	_ = mapUse(items)
 }
 
 func BenchmarkMapDynSeek(b *testing.B) {
 	for _, size := range testSizes {
 		b.Run(fmt.Sprintf("size=%d", size), func(b *testing.B) {
-			b.StopTimer()
-			items := mapFillDyn(size)
-			b.StartTimer()
-			for s := 0; s < size; s++ {
-				_, _ = mapSeek(items, allStrings[s])
+			benchmarkMapDynSeek(b, size)
+			if doGC {
+				runtime.GC()
 			}
 		})
+	}
+}
+
+func benchmarkMapDynSeek(b *testing.B, size int) {
+	b.StopTimer()
+	items := mapFillDyn(size)
+	b.StartTimer()
+	for s := 0; s < size; s++ {
+		_, _ = mapSeek(items, allStrings[s])
 	}
 }
 
 func BenchmarkMapFixSeek(b *testing.B) {
 	for _, size := range testSizes {
 		b.Run(fmt.Sprintf("size=%d", size), func(b *testing.B) {
-			b.StopTimer()
-			items := mapFillFix(size)
-			b.StartTimer()
-			for s := 0; s < size; s++ {
-				_, _ = mapSeek(items, allStrings[s])
+			benchmarkMapFixSeek(b, size)
+			if doGC {
+				runtime.GC()
 			}
 		})
+	}
+}
+
+func benchmarkMapFixSeek(b *testing.B, size int) {
+	b.StopTimer()
+	items := mapFillFix(size)
+	b.StartTimer()
+	for s := 0; s < size; s++ {
+		_, _ = mapSeek(items, allStrings[s])
 	}
 }
 
